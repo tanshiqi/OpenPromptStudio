924994807691c393e7239969c024feb548f57693
tanshiqi <tanshiqi@gmail.com>
2024-04-17 10:17:48 +0800

    cors

--- src/Compoents/PromptEditor/Lib/DatabaseServer/lib/fetchFromNotion.ts
+++ src/Compoents/PromptEditor/Lib/DatabaseServer/lib/fetchFromNotion.ts
@@ -16,7 +16,7 @@ export async function fetchFromNotion(options: { apiKey: string; databaseId: str
     const notion = new Client({
         auth: apiKey,
         // baseUrl: `https://cloudy-toad-cors-dgvpb2704n30.deno.dev/https://api.notion.com`,
-        baseUrl: `https://proxy.pinweb.io/proxy/api.notion.com`,
+        baseUrl: `https://cors.ledo.world/https://api.notion.com`,
     })
 
     let i = 0
